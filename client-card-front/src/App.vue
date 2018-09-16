1274d5b9579a7938dfab134276fd33639b431804
vanyakulinich <kulinichvanya@gmail.com>
2018-09-16 18:39:58 +0300

    material added and tried

--- client-card-front/src/App.vue
+++ client-card-front/src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <md-card>
+      <md-card-header>
+        header
+      </md-card-header>
+      <md-card-content>
+        content
+      </md-card-content>
+      <md-card-content>
+        content2
+      </md-card-content>
+      <md-card-actions>
+        <md-button>click</md-button>
+      </md-card-actions>
+    </md-card>
+    <md-list class="md-triple-line">
+      <md-list-item>
+        <md-avatar>
+          <img src="https://placeimg.com/40/40/people/1" alt="People">
+        </md-avatar>
+
+        <div class="md-list-item-text">
+          <span>Ali Connors</span>
+          <span>Brunch this weekend?</span>
+          <p>I'll be in your neighborhood doing errands this week. Do you want to meet?</p>
+        </div>
+
+        <md-button class="md-icon-button md-list-action">
+          <md-icon class="md-primary">star</md-icon>
+        </md-button>
+      </md-list-item>
+    </md-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // components: {
+    
+  // }
+
 }
 </script>
 
@@ -21,8 +50,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* display: flex; */
+  width: 80vw;
+  height: 80vh;
 }
 </style>
